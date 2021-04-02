6475f08d47ff0dd1f3426d212621c1ba1c1dd13f
Jody Kocis <jodykocis@Jodys-iMac.local>
2021-04-02 17:22:47 -0400

    Initial commit

---
+++ Process Scheduling Visualizer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Process Scheduling Visualizer
+//
+//  Created by Jody Kocis on 4/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Process Scheduling Visualizer/Process_Scheduling_VisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Process_Scheduling_VisualizerApp.swift
+//  Process Scheduling Visualizer
+//
+//  Created by Jody Kocis on 4/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Process_Scheduling_VisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
